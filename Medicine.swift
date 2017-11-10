9835244c136dce59068c4094bfcb0266b7607ebd
Akhil Mantha <akhilmantha@live.com>
2017-11-10 17:21:46 +0530

    source files added

---
+++ Medicine.swift
@@ -0,0 +1,17 @@
+//
+//  Medicine.swift
+//  MedTracker
+//
+//  Created by akhil mantha on 18/06/17.
+//  Copyright © 2017 akhil mantha. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Medicine: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
